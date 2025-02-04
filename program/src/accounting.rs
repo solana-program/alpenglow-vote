1e1546d333cadd4c804798726605fc12b6f98db4
Ashwin Sekar <ashwin@solana.com>
2025-02-04 04:39:13 +0000

    migrate the accounting instructions from original vote program
    We also remove the prior voters field and simplify the
    authorized voter storage and the epoch credits storage

---
+++ program/src/accounting.rs
@@ -0,0 +1,193 @@
+//! Accounting related operations on the Vote Account
+
+use bytemuck::{Pod, PodInOption, Zeroable, ZeroableInOption};
+use solana_program::account_info::AccountInfo;
+use solana_program::clock::Clock;
+use solana_program::clock::Slot;
+use solana_program::epoch_schedule::EpochSchedule;
+use solana_program::program_error::ProgramError;
+use solana_program::pubkey::Pubkey;
+use solana_program::rent::Rent;
+use spl_pod::bytemuck::pod_from_bytes_mut;
+use spl_pod::primitives::PodU64;
+
+use crate::error::VoteError;
+use crate::instruction::AuthorityType;
+use crate::state::{PodEpoch, VoteState};
+
+/// Authorized Signer for vote instructions
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Pod, Zeroable, Default, PartialEq)]
+pub struct AuthorizedVoter {
+    /// Epoch that is authorized
+    pub epoch: PodEpoch,
+    /// Voter that is authorized
+    pub voter: Pubkey,
+}
+
+// UNSAFE: we require that `epoch > 0` so this is safe
+unsafe impl ZeroableInOption for AuthorizedVoter {}
+unsafe impl PodInOption for AuthorizedVoter {}
+
+/// The credits information for an epoch
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Pod, Zeroable, Default, PartialEq)]
+pub struct EpochCredit {
+    /// Epoch in which credits were earned
+    pub epoch: PodEpoch,
+    /// Credits earned
+    pub credits: PodU64,
+    /// Credits earned in the previous epoch
+    pub prev_credits: PodU64,
+}
+
+/// Authorize the given pubkey to withdraw or sign votes. This may be called multiple times,
+/// but will implicitly withdraw authorization from the previously authorized key
+pub(crate) fn authorize(
+    vote_account: &AccountInfo,
+    new_authority: &Pubkey,
+    vote_authorize: AuthorityType,
+    authority: &Pubkey,
+    clock: &Clock,
+) -> Result<(), ProgramError> {
+    let mut buffer = vote_account.try_borrow_mut_data()?;
+    let vote_state = pod_from_bytes_mut::<VoteState>(&mut buffer)?;
+
+    match vote_authorize {
+        AuthorityType::Voter => {
+            // Current authorized withdrawer or voter must match
+            if vote_state.authorized_withdrawer != *authority
+                && vote_state.authorized_voter.voter != *authority
+            {
+                return Err(ProgramError::MissingRequiredSignature);
+            }
+
+            let epoch_in_effect = clock
+                .leader_schedule_epoch
+                .checked_add(1)
+                .ok_or(ProgramError::InvalidInstructionData)?;
+            // Overwrite the next authorized voter
+            vote_state.next_authorized_voter = Some(AuthorizedVoter {
+                epoch: PodU64::from(epoch_in_effect),
+                voter: *new_authority,
+            });
+        }
+        AuthorityType::Withdrawer => {
+            // Current authorized withdrawer must match
+            if vote_state.authorized_withdrawer != *authority {
+                return Err(ProgramError::MissingRequiredSignature);
+            }
+            vote_state.authorized_withdrawer = *new_authority;
+        }
+    }
+    Ok(())
+}
+
+pub(crate) fn withdraw(
+    vote_account: &AccountInfo,
+    recipient: &AccountInfo,
+    lamports: u64,
+    withdraw_pubkey: &Pubkey,
+    rent_sysvar: &Rent,
+    clock: &Clock,
+) -> Result<(), ProgramError> {
+    let vote_state = vote_account.data.borrow();
+    let vote_state = bytemuck::from_bytes::<VoteState>(&vote_state);
+
+    if vote_state.authorized_withdrawer != *withdraw_pubkey {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    let remaining_balance = vote_account
+        .try_lamports()?
+        .checked_sub(lamports)
+        .ok_or(ProgramError::InsufficientFunds)?;
+
+    if remaining_balance == 0 {
+        let last_epoch_with_credits = u64::from(vote_state.epoch_credits.epoch);
+        let current_epoch = clock.epoch;
+        // if current_epoch - last_epoch_with_credits < 2 then the validator has received credits
+        // either in the current epoch or the previous epoch. If it's >= 2 then it has been at least
+        // one full epoch since the validator has received credits.
+        let reject_active_vote_account_close =
+            current_epoch.saturating_sub(last_epoch_with_credits) < 2;
+
+        if reject_active_vote_account_close {
+            return Err(VoteError::ActiveVoteAccountClose.into());
+        } else {
+            // Deinitialize upon zero-balance
+            VoteState::set_vote_account_state(vote_account, &VoteState::default())?;
+        }
+    } else {
+        let min_rent_exempt_balance = rent_sysvar.minimum_balance(vote_account.data_len());
+        if remaining_balance < min_rent_exempt_balance {
+            return Err(ProgramError::InsufficientFunds);
+        }
+    }
+
+    let vote_account_lamports = vote_account.try_borrow_mut_lamports()?;
+    vote_account_lamports
+        .checked_sub(lamports)
+        .ok_or(ProgramError::InsufficientFunds)?;
+    let recipient_lamports = recipient.try_borrow_mut_lamports()?;
+    recipient_lamports
+        .checked_add(lamports)
+        .ok_or(ProgramError::ArithmeticOverflow)?;
+    Ok(())
+}
+
+pub(crate) fn update_validator_identity(
+    vote_account: &AccountInfo,
+    new_node_pubkey: &Pubkey,
+    withdraw_pubkey: &Pubkey,
+) -> Result<(), ProgramError> {
+    let mut buffer = vote_account.try_borrow_mut_data()?;
+    let vote_state = pod_from_bytes_mut::<VoteState>(&mut buffer)?;
+
+    if vote_state.authorized_withdrawer != *withdraw_pubkey {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    vote_state.node_pubkey = *new_node_pubkey;
+    Ok(())
+}
+
+pub(crate) fn update_commission(
+    vote_account: &AccountInfo,
+    commission: u8,
+    withdraw_pubkey: &Pubkey,
+    epoch_schedule: &EpochSchedule,
+    clock: &Clock,
+) -> Result<(), ProgramError> {
+    let mut buffer = vote_account.try_borrow_mut_data()?;
+    let vote_state = pod_from_bytes_mut::<VoteState>(&mut buffer)?;
+
+    if vote_state.authorized_withdrawer != *withdraw_pubkey {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    let is_commission_increase = commission > vote_state.commission;
+    if !is_commission_increase && !is_commission_update_allowed(clock.slot, epoch_schedule) {
+        return Err(VoteError::CommissionUpdateTooLate.into());
+    }
+
+    vote_state.commission = commission;
+
+    Ok(())
+}
+
+/// Given the current slot and epoch schedule, determine if a commission change
+/// is allowed
+fn is_commission_update_allowed(slot: Slot, epoch_schedule: &EpochSchedule) -> bool {
+    // always allowed during warmup epochs
+    if let Some(relative_slot) = slot
+        .saturating_sub(epoch_schedule.first_normal_slot)
+        .checked_rem(epoch_schedule.slots_per_epoch)
+    {
+        // allowed up to the midpoint of the epoch
+        relative_slot.saturating_mul(2) <= epoch_schedule.slots_per_epoch
+    } else {
+        // no slots per epoch, just allow it, even though this should never happen
+        true
+    }
+}
--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -2,36 +2,417 @@
 
 use {
     crate::{error::VoteError, id},
-    bytemuck::Pod,
+    bytemuck::{Pod, Zeroable},
     num_enum::{IntoPrimitive, TryFromPrimitive},
     solana_program::{
         instruction::{AccountMeta, Instruction},
         program_error::ProgramError,
+        pubkey::Pubkey,
+        sysvar,
     },
-    spl_pod::bytemuck::{pod_from_bytes, pod_get_packed_len},
+    spl_pod::{
+        bytemuck::{pod_bytes_of, pod_from_bytes, pod_get_packed_len},
+        primitives::{PodU32, PodU64},
+        slice::PodSlice,
+    },
 };
 
 /// Instructions supported by the program
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, TryFromPrimitive, IntoPrimitive)]
 pub enum VoteInstruction {
-    /// TODO: fill in with actual representation and update comments
-    NotarizationVote,
-    /// TODO: fill in with actual representation and update comments
-    SkipVote,
-    /// TODO: fill in with actual representation and update comments
-    FinalizationVote,
+    /// Initialize a vote account
+    ///
+    /// # Account references
+    ///   0. `[WRITE]` Uninitialized vote account
+    ///   1. `[]` Rent sysvar
+    ///   2. `[]` Clock sysvar
+    ///   3. `[SIGNER]` New validator identity (node_pubkey)
+    ///
+    ///   Data expected by this instruction:
+    ///     `InitializeAccountInstructionData`
+    InitializeAccount,
+
+    /// Authorize a key to send votes or issue a withdrawal
+    ///
+    /// # Account references
+    ///   0. `[WRITE]` Vote account to be updated with the Pubkey for authorization
+    ///   1. `[]` Clock sysvar
+    ///   2. `[SIGNER]` Vote or withdraw authority
+    ///
+    ///   Data expected by this instruction:
+    ///     `AuthorizeInstructionData`
+    Authorize,
+
+    /// Authorize a key to send votes or issue a withdrawal
+    ///
+    /// This instruction behaves like `Authorize` with the additional requirement that the new vote
+    /// or withdraw authority must also be a signer.
+    ///
+    /// # Account references
+    ///   0. `[WRITE]` Vote account to be updated with the Pubkey for authorization
+    ///   1. `[]` Clock sysvar
+    ///   2. `[SIGNER]` Vote or withdraw authority
+    ///   3. `[SIGNER]` New vote or withdraw authority
+    ///
+    ///   Data expected by this instruction:
+    ///     `VoteAuthorize`
+    AuthorizeChecked,
+
+    /// Given that the current Voter or Withdrawer authority is a derived key,
+    /// this instruction allows someone who can sign for that derived key's
+    /// base key to authorize a new Voter or Withdrawer for a vote account.
+    ///
+    /// # Account references
+    ///   0. `[Write]` Vote account to be updated
+    ///   1. `[]` Clock sysvar
+    ///   2. `[SIGNER]` Base key of current Voter or Withdrawer authority's derived key
+    ///
+    ///   Data expected by this instruction:
+    ///     `AuthorizeWithSeedInstructionData`
+    AuthorizeWithSeed,
+
+    /// Given that the current Voter or Withdrawer authority is a derived key,
+    /// this instruction allows someone who can sign for that derived key's
+    /// base key to authorize a new Voter or Withdrawer for a vote account.
+    ///
+    /// This instruction behaves like `AuthorizeWithSeed` with the additional requirement
+    /// that the new vote or withdraw authority must also be a signer.
+    ///
+    /// # Account references
+    ///   0. `[Write]` Vote account to be updated
+    ///   1. `[]` Clock sysvar
+    ///   2. `[SIGNER]` Base key of current Voter or Withdrawer authority's derived key
+    ///   3. `[SIGNER]` New vote or withdraw authority
+    ///
+    ///   Data expected by this instruction:
+    ///     `AuthorizeCheckedWithSeedInstructionData`
+    AuthorizeCheckedWithSeed,
+
+    /// Withdraw some amount of funds
+    ///
+    /// # Account references
+    ///   0. `[WRITE]` Vote account to withdraw from
+    ///   1. `[WRITE]` Recipient account
+    ///   2. `[]` Rent sysvar
+    ///   3. `[]` Clock sysvar
+    ///   4. `[SIGNER]` Withdraw authority
+    ///
+    ///   Data expected by this instruction:
+    ///     `lamports` : `u64`
+    Withdraw,
+
+    /// Update the vote account's validator identity (node_pubkey)
+    ///
+    /// # Account references
+    ///   0. `[WRITE]` Vote account to be updated with the given authority public key
+    ///   1. `[SIGNER]` New validator identity (node_pubkey)
+    ///   2. `[SIGNER]` Withdraw authority
+    UpdateValidatorIdentity,
+
+    /// Update the commission for the vote account
+    ///
+    /// # Account references
+    ///   0. `[WRITE]` Vote account to be updated
+    ///   1. `[]` Epoch schedule sysvar
+    ///   2. `[]` Clock sysvar
+    ///   3. `[SIGNER]` Withdraw authority
+    ///
+    ///   Data expected by this instruction:
+    ///     `commission` : `u8`
+    UpdateCommission,
+}
+
+/// Data expected by
+/// `VoteInstruction::InitializeAccount`
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Pod, Zeroable)]
+pub struct InitializeAccountInstructionData {
+    /// The node that votes in this account
+    pub node_pubkey: Pubkey,
+    /// The signer for vote transactions
+    pub authorized_voter: Pubkey,
+    /// The signer for withdrawals
+    pub authorized_withdrawer: Pubkey,
+    /// The commission percentage for this vote account
+    pub commission: u8,
+}
+
+/// Instruction builder to initialize a new vote account with a valid VoteState:
+/// - `vote_pubkey` the vote account
+pub fn initialize_account(
+    vote_pubkey: Pubkey,
+    instruction_data: &InitializeAccountInstructionData,
+) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new(vote_pubkey, false),
+        AccountMeta::new_readonly(sysvar::rent::id(), false),
+        AccountMeta::new_readonly(sysvar::clock::id(), false),
+        AccountMeta::new_readonly(instruction_data.node_pubkey, true),
+    ];
+
+    encode_instruction(
+        accounts,
+        VoteInstruction::InitializeAccount,
+        instruction_data,
+    )
+}
+
+/// The type of authority on the account
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, TryFromPrimitive, IntoPrimitive)]
+pub enum AuthorityType {
+    /// Voting authority
+    Voter,
+    /// Withdrawal authority
+    Withdrawer,
+}
+
+/// Data expected by
+/// `VoteInstruction::Authorize`
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Pod, Zeroable)]
+pub struct AuthorizeInstructionData {
+    /// New authority pubkey for the vote account
+    pub new_authorized_pubkey: Pubkey,
+    /// The type of authority
+    pub authority_type: u8,
+}
+
+/// Instruction builder to update the authority of a vote account
+/// - `vote_pubkey` the vote account
+/// - `authorized_pubkey` the current authority
+/// - `new_authorized_pubkey` the new authority
+/// - `authority_type` the type of the authorities
+pub fn authorize(
+    vote_pubkey: Pubkey,
+    authorized_pubkey: Pubkey, // currently authorized
+    new_authorized_pubkey: Pubkey,
+    authority_type: AuthorityType,
+) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new(vote_pubkey, false),
+        AccountMeta::new_readonly(sysvar::clock::id(), false),
+        AccountMeta::new_readonly(authorized_pubkey, true),
+    ];
+
+    encode_instruction(
+        accounts,
+        VoteInstruction::Authorize,
+        &AuthorizeInstructionData {
+            new_authorized_pubkey,
+            authority_type: u8::from(authority_type),
+        },
+    )
+}
+
+/// Instruction builder to update the authority of a vote account
+/// This checked variant requires `new_authorized_pubkey` to be a signer
+/// - `vote_pubkey` the vote account
+/// - `authorized_pubkey` the current authority
+/// - `new_authorized_pubkey` the new authority
+/// - `vote_authorize` the type of the authorities
+pub fn authorize_checked(
+    vote_pubkey: Pubkey,
+    authorized_pubkey: Pubkey, // currently authorized
+    new_authorized_pubkey: Pubkey,
+    authority_type: AuthorityType,
+) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new(vote_pubkey, false),
+        AccountMeta::new_readonly(sysvar::clock::id(), false),
+        AccountMeta::new_readonly(authorized_pubkey, true),
+        AccountMeta::new_readonly(new_authorized_pubkey, true),
+    ];
+
+    encode_instruction(
+        accounts,
+        VoteInstruction::AuthorizeChecked,
+        &u8::from(authority_type),
+    )
+}
+
+/// Data expected by
+/// `VoteInstruction::AuthorizeWithSeed`
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, Pod, Zeroable)]
+pub struct AuthorizeWithSeedInstructionData {
+    /// The authority type
+    pub authority_type: u8,
+    /// The current authority owner key
+    pub current_authority_derived_key_owner: Pubkey,
+    /// The new authority pubkey for the vote account
+    pub new_authority: Pubkey,
+}
+
+/// Instruction builder to update the authority of a vote account
+/// using a seed based schema
+/// - `vote_pubkey` the vote account
+/// - `current_authority_base_key` the base key of the current authority
+/// - `current_authority_derived_key_owner` current authority owner
+/// - `current_authority_derived_key_seed` current authority seed
+/// - `new_authorized_pubkey` the new authority
+/// - `authority_type` the type of the authorities
+pub fn authorize_with_seed(
+    vote_pubkey: Pubkey,
+    current_authority_base_key: Pubkey,
+    current_authority_derived_key_owner: Pubkey,
+    current_authority_derived_key_seed: &str,
+    new_authority: Pubkey,
+    authority_type: AuthorityType,
+) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new(vote_pubkey, false),
+        AccountMeta::new_readonly(sysvar::clock::id(), false),
+        AccountMeta::new_readonly(current_authority_base_key, true),
+    ];
+
+    encode_instruction_with_seed(
+        accounts,
+        VoteInstruction::AuthorizeWithSeed,
+        &AuthorizeWithSeedInstructionData {
+            authority_type: u8::from(authority_type),
+            current_authority_derived_key_owner,
+            new_authority,
+        },
+        Some(current_authority_derived_key_seed),
+    )
+}
+
+/// Data expected by
+/// `VoteInstruction::AuthorizeCheckedWithSeed`
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, Pod, Zeroable)]
+pub struct AuthorizeCheckedWithSeedInstructionData {
+    /// The authority type
+    pub authority_type: u8,
+    /// The current authority owner key
+    pub current_authority_derived_key_owner: Pubkey,
+}
+
+/// Instruction builder to update the authority of a vote account
+/// using a seed based schema
+/// This checked variant requires `new_authorized_pubkey` to be a signer
+/// - `vote_pubkey` the vote account
+/// - `current_authority_base_key` the base key of the current authority
+/// - `current_authority_derived_key_owner` current authority owner
+/// - `current_authority_derived_key_seed` current authority seed
+/// - `new_authorized_pubkey` the new authority
+/// - `authority_type` the type of the authorities
+pub fn authorize_checked_with_seed(
+    vote_pubkey: Pubkey,
+    current_authority_base_key: Pubkey,
+    current_authority_derived_key_owner: Pubkey,
+    current_authority_derived_key_seed: &str,
+    new_authority: Pubkey,
+    authorization_type: AuthorityType,
+) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new(vote_pubkey, false),
+        AccountMeta::new_readonly(sysvar::clock::id(), false),
+        AccountMeta::new_readonly(current_authority_base_key, true),
+        AccountMeta::new_readonly(new_authority, true),
+    ];
+
+    encode_instruction_with_seed(
+        accounts,
+        VoteInstruction::AuthorizeWithSeed,
+        &AuthorizeCheckedWithSeedInstructionData {
+            authority_type: u8::from(authorization_type),
+            current_authority_derived_key_owner,
+        },
+        Some(current_authority_derived_key_seed),
+    )
+}
+
+/// Instruction builder to withdraw from the vote account
+/// - `vote_pubkey` the vote account
+/// - `authorized_withdrawer_pubkey` the withdraw authority of the vote account
+/// - `lamports` amount to withdraw
+/// - `recipient` the account to withdraw to
+pub fn withdraw(
+    vote_pubkey: Pubkey,
+    authorized_withdrawer_pubkey: Pubkey,
+    lamports: u64,
+    recipient: Pubkey,
+) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new(vote_pubkey, false),
+        AccountMeta::new(recipient, false),
+        AccountMeta::new_readonly(authorized_withdrawer_pubkey, true),
+    ];
+
+    encode_instruction(accounts, VoteInstruction::Withdraw, &PodU64::from(lamports))
+}
+
+/// Instruction builder to update the node pubkey on the vote account
+/// - `vote_pubkey` the vote account
+/// - `authorized_withdrawer_pubkey` the withdraw authority of the vote account
+/// - `node_pubkey` the new node pubkey to write to the vote account
+pub fn update_validator_identity(
+    vote_pubkey: Pubkey,
+    authorized_withdrawer_pubkey: Pubkey,
+    node_pubkey: Pubkey,
+) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new(vote_pubkey, false),
+        AccountMeta::new_readonly(node_pubkey, true),
+        AccountMeta::new_readonly(authorized_withdrawer_pubkey, true),
+    ];
+
+    let data = vec![u8::from(VoteInstruction::UpdateValidatorIdentity)];
+
+    Instruction {
+        program_id: id(),
+        accounts,
+        data,
+    }
+}
+
+/// Instruction builder to update the commission on the vote account
+/// - `vote_pubkey` the vote account
+/// - `authorized_withdrawer_pubkey` the withdraw authority of the vote account
+/// - `commission`  the new commission to write to the vote account
+pub fn update_commission(
+    vote_pubkey: Pubkey,
+    authorized_withdrawer_pubkey: Pubkey,
+    commission: u8,
+) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new(vote_pubkey, false),
+        AccountMeta::new_readonly(authorized_withdrawer_pubkey, true),
+    ];
+
+    encode_instruction(accounts, VoteInstruction::UpdateCommission, &commission)
 }
 
 /// Utility function for encoding instruction data
-#[allow(dead_code)]
 pub(crate) fn encode_instruction<D: Pod>(
     accounts: Vec<AccountMeta>,
     instruction: VoteInstruction,
     instruction_data: &D,
 ) -> Instruction {
+    encode_instruction_with_seed(accounts, instruction, instruction_data, None)
+}
+
+/// Utility function for encoding instruction data
+/// with a seed.
+///
+/// Some accounting instructions have a variable length
+/// `seed`, we serialize this as a pod slice at the end
+/// of the instruction data
+pub(crate) fn encode_instruction_with_seed<D: Pod>(
+    accounts: Vec<AccountMeta>,
+    instruction: VoteInstruction,
+    instruction_data: &D,
+    seed: Option<&str>,
+) -> Instruction {
     let mut data = vec![u8::from(instruction)];
     data.extend_from_slice(bytemuck::bytes_of(instruction_data));
+    if let Some(seed) = seed {
+        let seed_len = PodU32::from(seed.len() as u32);
+        data.extend_from_slice(&[pod_bytes_of(&seed_len), seed.as_bytes()].concat());
+    }
     Instruction {
         program_id: id(),
         accounts,
@@ -40,7 +421,6 @@ pub(crate) fn encode_instruction<D: Pod>(
 }
 
 /// Utility function for decoding just the instruction type
-#[allow(dead_code)]
 pub(crate) fn decode_instruction_type(input: &[u8]) -> Result<VoteInstruction, ProgramError> {
     if input.is_empty() {
         Err(ProgramError::InvalidInstructionData)
@@ -50,11 +430,26 @@ pub(crate) fn decode_instruction_type(input: &[u8]) -> Result<VoteInstruction, P
 }
 
 /// Utility function for decoding instruction data
-#[allow(dead_code)]
 pub(crate) fn decode_instruction_data<T: Pod>(input_with_type: &[u8]) -> Result<&T, ProgramError> {
     if input_with_type.len() != pod_get_packed_len::<T>().saturating_add(1) {
         Err(ProgramError::InvalidInstructionData)
     } else {
         pod_from_bytes(&input_with_type[1..])
     }
+}
+
+/// Utility function for decoding instruction data with a variable length seed
+pub(crate) fn decode_instruction_data_with_seed<T: Pod>(
+    input_with_type: &[u8],
+) -> Result<(&T, PodSlice<'_, u8>), ProgramError> {
+    if input_with_type.len() < pod_get_packed_len::<T>().saturating_add(1) {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+
+    let data_offset = std::mem::size_of::<T>()
+        .checked_add(1)
+        .ok_or(ProgramError::ArithmeticOverflow)?;
+    let instruction_data = pod_from_bytes(&input_with_type[1..data_offset])?;
+    let seed = PodSlice::unpack(&input_with_type[data_offset..])?;
+    Ok((instruction_data, seed))
 }
