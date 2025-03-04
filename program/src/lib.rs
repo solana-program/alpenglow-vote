0572a0cd4e602b6ae465a14eb9847bfb2bad01da
Ashwin Sekar <ashwin@solana.com>
2025-03-04 21:44:47 +0000

    interface: expose serde and abi example for client types

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,5 +1,8 @@
 //! Alpenglow Vote program
 #![deny(missing_docs)]
+// Magic to enable frozen abi for on chain programs
+#![cfg_attr(feature = "frozen-abi", feature(min_specialization))]
+
 pub mod accounting;
 mod entrypoint;
 pub mod error;
@@ -14,3 +17,7 @@ mod vote_processor;
 pub use solana_program;
 
 solana_program::declare_id!("Vote222222222222222222222222222222222222222");
+
+#[cfg_attr(feature = "frozen-abi", macro_use)]
+#[cfg(feature = "frozen-abi")]
+extern crate solana_frozen_abi_macro;
