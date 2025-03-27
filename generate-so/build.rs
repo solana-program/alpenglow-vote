96f8d34cadd64b374c10606c57261df608083f76
ksn6 <karthik@anza.xyz>
2025-03-27 00:23:21 -0400

    feat: commit `alpenglow_vote.so` for easier usage in `alpenglow` (#53)
    * feat: commit alpenglow_vote.so.
    
    * build.rs: auto-generate spl-alpenglow_vote.so.
    
    * style: clean generate-so/Cargo.toml.
    
    * fix generate-so bugs.
    
    * things work finally

---
+++ generate-so/build.rs
@@ -0,0 +1,27 @@
+use std::{env, fs, path::PathBuf};
+
+fn main() {
+    // Get project directory
+    let base_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+        .parent()
+        .unwrap()
+        .to_path_buf();
+
+    // src: project-dir/target/deploy/alpenglow_vote.so
+    let deploy_so_path = base_path
+        .join("target")
+        .join("deploy")
+        .join("alpenglow_vote.so");
+
+    // dest: project-dir/spl-alpenglow_vote.so
+    let dest_path = base_path.join("spl-alpenglow_vote.so");
+
+    // Save the destination path as an environment variable that can later be invoked in Rust code
+    println!(
+        "cargo:rustc-env=ALPENGLOW_VOTE_SO_PATH={}",
+        dest_path.display()
+    );
+
+    // copy from src to dest
+    fs::copy(deploy_so_path, dest_path).expect("Couldn't copy spl-alpenglow_vote.so.");
+}
---
+++ generate-so/src/lib.rs
@@ -0,0 +1,14 @@
+/// Path to the alpenglow-vote shared object
+pub const ALPENGLOW_VOTE_SO_PATH: &'static str = env!("ALPENGLOW_VOTE_SO_PATH");
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use crate::ALPENGLOW_VOTE_SO_PATH;
+
+    #[test]
+    pub fn ensure_alpenglow_vote_so_path_exists() {
+        assert!(Path::new(ALPENGLOW_VOTE_SO_PATH).exists());
+    }
+}
