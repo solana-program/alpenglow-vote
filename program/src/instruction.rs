42192a69cb18c8be0c4e3f981f5aead0ed01fb0e
ksn6 <2163784+ksn6@users.noreply.github.com>
2025-04-03 18:33:55 -0400

    feat(instruction): introduce create_account_with_config (#58)
    * feat(instruction): introduce create_account_with_config.
    
    * style: minor comment edits.
    
    * addressing pr comments

--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -4,7 +4,7 @@ use {
     crate::{
         error::VoteError,
         id,
-        state::{PodSlot, PodUnixTimestamp},
+        state::{PodSlot, PodUnixTimestamp, VoteState},
         vote::{FinalizationVote, NotarizationVote, SkipVote},
         vote_processor::{
             FinalizationVoteInstructionData, NotarizationVoteInstructionData,
@@ -18,6 +18,8 @@ use {
         instruction::{AccountMeta, Instruction},
         program_error::ProgramError,
         pubkey::Pubkey,
+        rent::Rent,
+        system_instruction,
     },
     spl_pod::{
         bytemuck::{pod_bytes_of, pod_from_bytes, pod_get_packed_len},
@@ -237,6 +239,7 @@ pub struct InitializeAccountInstructionData {
 
 /// Instruction builder to initialize a new vote account with a valid VoteState:
 /// - `vote_pubkey` the vote account
+/// - `instruction_data` the vote account's account creation metadata
 pub fn initialize_account(
     vote_pubkey: Pubkey,
     instruction_data: &InitializeAccountInstructionData,
@@ -251,6 +254,30 @@ pub fn initialize_account(
         VoteInstruction::InitializeAccount,
         instruction_data,
     )
+}
+
+/// Instruction builder to create and initialize a new vote account with a valid VoteState:
+/// - `from_pubkey` the account that funds the rent exemption
+/// - `vote_pubkey` the vote account
+/// - `rent` network Rent details
+/// - `instruction_data` the vote account's account creation metadata
+pub fn create_account_with_config(
+    from_pubkey: &Pubkey,
+    vote_pubkey: &Pubkey,
+    rent: &Rent,
+    instruction_data: InitializeAccountInstructionData,
+) -> Vec<Instruction> {
+    let create_ix = system_instruction::create_account(
+        from_pubkey,
+        vote_pubkey,
+        rent.minimum_balance(VoteState::size()),
+        VoteState::size() as u64,
+        &id(),
+    );
+
+    let init_ix = initialize_account(*vote_pubkey, &instruction_data);
+
+    vec![create_ix, init_ix]
 }
 
 /// The type of authority on the account
