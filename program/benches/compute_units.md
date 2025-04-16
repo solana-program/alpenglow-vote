878eb44c8320ba2999e9e35ab56e4d9aecd88d54
Joe Caulfield <jcaulfield135@gmail.com>
2025-04-16 16:04:25 +0000

    benchmark compute units

---
+++ program/benches/compute_units.md
@@ -0,0 +1,10 @@
+#### 2025-04-16 16:03:31.270151200 UTC
+
+Solana CLI Version: solana-cli 2.2.0 (src:8b11c7d5; feat:3294202862, client:Agave)
+
+| Name | CUs | Delta |
+|------|------|-------|
+| finalize | 1588 | - new - |
+| notarize | 1586 | - new - |
+| skip | 1573 | - new - |
+
---
+++ program/benches/compute_units.rs
@@ -0,0 +1,75 @@
+//! Alpenglow compute unit benchmark testing.
+
+use {
+    alpenglow_vote::{
+        instruction::{finalize, notarize, skip},
+        state::VoteState,
+        vote::{FinalizationVote, NotarizationVote, SkipVote},
+    },
+    mollusk_svm::Mollusk,
+    mollusk_svm_bencher::MolluskComputeUnitBencher,
+    solana_sdk::{account::Account, pubkey::Pubkey},
+};
+
+const BENCHMARK_OUT_DIR: &str = "./benches";
+const SBF_OUT_DIR: &str = "../target/deploy";
+
+fn vote_account(authority: &Pubkey) -> Account {
+    VoteState::create_account_with_authorized(&Pubkey::new_unique(), authority, authority, 0, 0)
+        .into()
+}
+
+fn main() {
+    std::env::set_var("SBF_OUT_DIR", SBF_OUT_DIR);
+
+    let mut mollusk = Mollusk::new(&alpenglow_vote::id(), "alpenglow_vote");
+
+    let slot = 5;
+    mollusk.warp_to_slot(slot + 1);
+
+    let bank_hash = *mollusk.sysvars.slot_hashes.get(&slot).unwrap();
+
+    MolluskComputeUnitBencher::new(mollusk)
+        .bench({
+            let vote_address = Pubkey::new_unique();
+            let authority = Pubkey::new_unique();
+            let vote = FinalizationVote::new(slot, bank_hash, slot, bank_hash);
+            (
+                "finalize",
+                &finalize(vote_address, authority, &vote),
+                &[
+                    (vote_address, vote_account(&authority)),
+                    (authority, Account::default()),
+                ],
+            )
+        })
+        .bench({
+            let vote_address = Pubkey::new_unique();
+            let authority = Pubkey::new_unique();
+            let vote = NotarizationVote::new(slot, bank_hash, slot, bank_hash, None);
+            (
+                "notarize",
+                &notarize(vote_address, authority, &vote),
+                &[
+                    (vote_address, vote_account(&authority)),
+                    (authority, Account::default()),
+                ],
+            )
+        })
+        .bench({
+            let vote_address = Pubkey::new_unique();
+            let authority = Pubkey::new_unique();
+            let vote = SkipVote::new(slot, slot);
+            (
+                "skip",
+                &skip(vote_address, authority, &vote),
+                &[
+                    (vote_address, vote_account(&authority)),
+                    (authority, Account::default()),
+                ],
+            )
+        })
+        .must_pass(true)
+        .out_dir(BENCHMARK_OUT_DIR)
+        .execute();
+}
