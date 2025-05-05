dbc98b3972553b21025f4293090c14f313060dab
Wen <113942165+wen-coding@users.noreply.github.com>
2025-05-05 15:48:49 -0700

    Define BLSMessage in vote program so everyone can serialize/deserialize other's packets.

---
+++ program/src/bls_message.rs
@@ -0,0 +1,42 @@
+//! Put BLS message here so all clients can agree on the format
+
+use {
+    crate::{
+        certificate::{Certificate, CertificateType},
+        vote::Vote,
+    },
+    solana_hash::Hash,
+    solana_program::clock::Slot,
+};
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Clone, Copy, Debug, PartialEq)]
+/// BLS message type in Alpenglow
+pub enum BlsMessage {
+    /// Vote message
+    Vote(Vote),
+    /// Certificate message
+    Certificate(Certificate),
+}
+
+impl BlsMessage {
+    /// Create a new vote message
+    pub fn new_vote(vote: Vote) -> Self {
+        Self::Vote(vote)
+    }
+
+    /// Create a new certificate message
+    pub fn new_certificate(
+        certificate_type: CertificateType,
+        slot: Slot,
+        block_id: Option<Hash>,
+        replayed_bank_hash: Option<Hash>,
+    ) -> Self {
+        Self::Certificate(Certificate {
+            certificate_type,
+            slot,
+            block_id,
+            replayed_bank_hash,
+        })
+    }
+}
--- program/src/certificate.rs
+++ program/src/certificate.rs
@@ -1,6 +1,9 @@
-//! Define certificate types here for now, maybe move later if we process
-//! certificates off chain
+//! Define BLS certificate to be sent all to all in Alpenglow
 
+use solana_hash::Hash;
+use solana_program::clock::Slot;
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Clone, Copy, Debug, PartialEq)]
 /// Certificate Type in Alpenglow
 pub enum CertificateType {
@@ -14,4 +17,19 @@ pub enum CertificateType {
     NotarizeFallback,
     /// Skip: at least 60 percent Skip or SkipFallback
     Skip,
+}
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Clone, Copy, Debug, PartialEq)]
+/// Certificate Type in Alpenglow
+pub struct Certificate {
+    /// Certificate type
+    pub certificate_type: CertificateType,
+    /// The slot of the block
+    pub slot: Slot,
+    /// The block id of the block
+    pub block_id: Option<Hash>,
+    /// The bank hash of the block
+    pub replayed_bank_hash: Option<Hash>,
+    //TODO(wen): add BLS signature, pubkey, and bitmap
 }
